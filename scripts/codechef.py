9f7ca4e749333a2b540fb08a0405746b5faa6ba3
Mizan <mdmizanali04@gmail.com>
2022-03-26 18:35:45 +0530

    added codechef.oy

---
+++ scripts/codechef.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+
+def fetch_codechef_data(username):
+    try:
+        response = requests.get(
+            'https://codechef-userdetails-api.herokuapp.com/' + username)
+        response_info = response.json()
+        if 'error' in response_info:
+            return {"SUCCESS": False}
+        return response_info
+    except:
+        return {"SUCCESS": False}
+
+
+if __name__ == "__main__":
+    print(fetch_codechef_data("tourist"))
