2a61975d4a71c7fb7a01004373d3c8c7ab829334
Jayesh Rohan Singh <rohan.jayesh18@gmail.com>
2022-03-27 14:29:46 +0530

    [fixed] minor bugs

--- website/__init__.py
+++ website/__init__.py
@@ -17,9 +17,11 @@ def create_app():
     
     from .views import views
     from .auth import auth
+    from .modify import modify
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(modify, url_prefix='/')
     
     from .models import UserDB
     create_database(app)
