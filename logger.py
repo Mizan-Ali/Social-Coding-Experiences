8c0f80c27179fe0f70a51aa96a32de7bc90f612a
Conero007 <rohan.jayesh18@gmail.com>
2022-04-13 10:29:27 +0530

    [formatted] black

--- logger.py
+++ logger.py
@@ -2,38 +2,39 @@
 
 
 class Logger:
-    '''Logger class for OUTLIERS app. Support for debug and error message handling'''
+    """Logger class for OUTLIERS app. Support for debug and error message handling"""
+
     __DEBUGLVL = 0
-    
+
     def debug(self, debuglvl, function_name, debug_message, **kwargs):
         if self.__DEBUGLVL >= debuglvl:
-            print(f'DEBUG   {function_name}   {debug_message}')
+            print(f"DEBUG   {function_name}   {debug_message}")
 
         if self.__DEBUGLVL >= 5 and kwargs:
-            print(f'DEBUG   DUMP:')
-            
+            print(f"DEBUG   DUMP:")
+
             for key, value in kwargs.items():
-                if 'password' == key:
+                if "password" == key:
                     continue
-                print(f'DEBUG          {key} : {value}')
+                print(f"DEBUG          {key} : {value}")
 
     def error(self, debuglvl, function_name, error_message, **kwargs):
         if self.__DEBUGLVL >= debuglvl:
-            print(f'ERROR   {function_name}   {error_message}')
+            print(f"ERROR   {function_name}   {error_message}")
 
         if self.__DEBUGLVL >= 5 and kwargs:
-            print(f'ERROR   DUMP:')
-        
+            print(f"ERROR   DUMP:")
+
             for key, value in kwargs.items():
-                if 'password' == key:
+                if "password" == key:
                     continue
-                print(f'ERROR          {key} : {value}')
-    
+                print(f"ERROR          {key} : {value}")
+
     def scheduler(self, scheduler_obj):
         scheduler_obj.print_jobs()
-    
+
     def set_debug_lvl(self, debug_lvl):
         Logger.__DEBUGLVL = int(debug_lvl)
-    
+
     def get_debug_lvl(self):
         return Logger.__DEBUGLVL
